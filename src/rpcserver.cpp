7c068f83c1a3b97427c8770a62e46af54ce7054c
Braydon Fuller <courier@braydon.com>
2016-03-17 16:06:08 -0400

    rpc: added getaddressbalance method using addressindex

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -333,6 +333,7 @@ static const CRPCCommand vRPCCommands[] =
 
     /* Address index */
     { "addressindex",       "getaddresstxids",        &getaddresstxids,        false },
+    { "addressindex",       "getaddressbalance",      &getaddressbalance,      false },
 
     /* Utility functions */
     { "util",               "createmultisig",         &createmultisig,         true  },
