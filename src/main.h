0f965536bf0a7d2558f960c3f9fb5782534793d9
Braydon Fuller <courier@braydon.com>
2016-03-30 16:42:37 -0400

    main: sort address index utxos by height

--- src/main.h
+++ src/main.h
@@ -404,6 +404,7 @@ struct CAddressUnspentKey {
 struct CAddressUnspentValue {
     CAmount satoshis;
     CScript script;
+    int blockHeight;
 
     ADD_SERIALIZE_METHODS;
 
@@ -411,11 +412,13 @@ struct CAddressUnspentValue {
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
         READWRITE(satoshis);
         READWRITE(*(CScriptBase*)(&script));
+        READWRITE(blockHeight);
     }
 
-    CAddressUnspentValue(CAmount sats, CScript scriptPubKey) {
+    CAddressUnspentValue(CAmount sats, CScript scriptPubKey, int height) {
         satoshis = sats;
         script = scriptPubKey;
+        blockHeight = height;
     }
 
     CAddressUnspentValue() {
@@ -425,6 +428,7 @@ struct CAddressUnspentValue {
     void SetNull() {
         satoshis = -1;
         script.clear();
+        blockHeight = 0;
     }
 
     bool IsNull() const {
